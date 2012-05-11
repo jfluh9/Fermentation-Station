42cd2236e0866faa1d532f06973c901f737ed4b3
Jared Rothenfluh <jfluh9@gmail.com>
2012-05-10 21:31:37 -0700

    fade in for the login
    got the fade for the login to work!

--- scripts/jQuery/test_jquery.js
+++ scripts/jQuery/test_jquery.js
@@ -3,8 +3,8 @@ $(document) .ready(function() {
     $("#dialog") .dialog( {
         modal: true,
         minWidth: 500,
-        minHeight: 300,
-    });
+        minHeight: 300, 
+    }); $(".ui-widget") .hide() .fadeIn(1000);
 });
 
 
