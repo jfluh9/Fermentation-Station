080e66dd925655cdb8b406383039af141dd38d37
Jared Rothenfluh <jfluh9@gmail.com>
2012-05-14 19:33:34 -0700

    Changes from work
    mainly CSS3 additions to h1, and also some site wide color changes.

--- scripts/jQuery/test_jquery.js
+++ scripts/jQuery/test_jquery.js
@@ -4,8 +4,24 @@ $(document) .ready(function() {
         modal: true,
         minWidth: 500,
         minHeight: 300, 
-    }); $(".ui-widget") .hide() .fadeIn(1000);
+    });
+    
+        $(".ui-widget") .hide() .fadeIn(1000); //On Load the login section fades into view
+
+            $("#usrName") .focus(); //On load the focus is set to the usrName input form
+
 });
+//The following function checks the form after every keypress.
+//Once the user name input is 5 characters in length it moves the focus
+//to the password input...and then onto the submit button.
+$(document) .keyup(function () {
 
+if(document.frmLogin.usrName.value.length>=5) {
+    $("#pwd") .focus();
+}
 
+if(document.frmLogin.pwd.value.length>=14) {
+    $("#submit") .focus();
+}
 
+});
